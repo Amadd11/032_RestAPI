b3e7098e90c24c4493542922d6a3413bb46c7049
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-26 20:42:13 +0700

    Menambahkan Object di HomeScreen

--- app/src/main/java/com/example/consumerestapi/ui/theme/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/theme/home/screen/HomeScreen.kt
@@ -3,8 +3,14 @@ package com.example.consumerestapi.ui.theme.home.screen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumerestapi.navigation.DestinasiNavigasi
 import com.example.consumerestapi.ui.theme.home.viewmodel.HomeViewModel
 import com.example.consumerestapi.ui.theme.theme.PenyediaViewModel
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Kontak"
+}
 
 @Composable
 fun HomeScreen(
