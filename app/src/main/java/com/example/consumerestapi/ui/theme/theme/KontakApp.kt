68eb7d76d4953c788ea56828e55ba74d2fb140f1
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-26 19:34:01 +0700

    Menambahkan Function TopAppBarKontak

---
+++ app/src/main/java/com/example/consumerestapi/ui/theme/theme/KontakApp.kt
@@ -0,0 +1,75 @@
+package com.example.consumerestapi.ui.theme.theme
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumerestapi.R
+import com.example.consumerestapi.ui.theme.home.screen.HomeScreen
+import com.example.consumerestapi.ui.theme.home.viewmodel.HomeViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KontakApp(
+    homeViewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    Scaffold (
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { TopAppBarKontak(scrollBehavior = scrollBehavior, title = "null", canNavigateBack = true) }
+    ){
+        Surface (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            HomeScreen(
+                kontakUIState = homeViewModel.kontakUIState,
+                retryAction = homeViewModel::getKontak
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarKontak(
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    title: String,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        modifier = modifier,
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+                }
+            }
+        }
+    )
+}
